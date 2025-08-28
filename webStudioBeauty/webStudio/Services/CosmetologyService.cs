b10d1c94768ed0a1f0fcf9eb486bb7e398b66a70
Vasyl Stolyarchuk <Vasyl.Stolyarchuk@outlook.com>
2025-08-28 23:57:47 +0300

    added new cosmetology service

---
+++ webStudioBeauty/webStudio/Services/CosmetologyService.cs
@@ -0,0 +1,13 @@
+﻿using webStudio.Data;
+
+namespace webStudio.Services
+{
+    public class CosmetologyService
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        public CosmetologyService(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+    }
+}
