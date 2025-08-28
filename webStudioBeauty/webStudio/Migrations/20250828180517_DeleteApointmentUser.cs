c07ae2c5e350a14a6860a9d5512f80b42f732432
Vasyl Stolyarchuk <Vasyl.Stolyarchuk@outlook.com>
2025-08-28 21:27:08 +0300

    fixed model entity and added migration

---
+++ webStudioBeauty/webStudio/Migrations/20250828180517_DeleteApointmentUser.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webStudio.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeleteApointmentUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categories_AppointmentUsers_UserId1",
+                table: "Categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_UserId1",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "UserId1",
+                table: "Categories");
+
+            migrationBuilder.AddColumn<int>(
+                name: "AppointmentUserId",
+                table: "Categories",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_AppointmentUserId",
+                table: "Categories",
+                column: "AppointmentUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categories_AppointmentUsers_AppointmentUserId",
+                table: "Categories",
+                column: "AppointmentUserId",
+                principalTable: "AppointmentUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categories_AppointmentUsers_AppointmentUserId",
+                table: "Categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_AppointmentUserId",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "AppointmentUserId",
+                table: "Categories");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId1",
+                table: "Categories",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_UserId1",
+                table: "Categories",
+                column: "UserId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categories_AppointmentUsers_UserId1",
+                table: "Categories",
+                column: "UserId1",
+                principalTable: "AppointmentUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- webStudioBeauty/webStudio/Models/Category.cs
+++ webStudioBeauty/webStudio/Models/Category.cs
@@ -8,8 +8,6 @@ namespace webStudio.Models
         public string NameCategory { get; set; }
         public List<CosmetologyTherapy> Cosmetologies { get; set; } = new List<CosmetologyTherapy>();
         public List<MessageTherapy> Messages { get; set; } = new List<MessageTherapy>();
-
         public string UserId { get; set; }
-        public AppointmentUser User { get; set; } = default!;
     }
 }
