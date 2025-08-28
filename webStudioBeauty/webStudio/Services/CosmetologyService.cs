67df09e2356018cb6b67daaca32ae408e9ca5b61
Vasyl Stolyarchuk <Vasyl.Stolyarchuk@outlook.com>
2025-08-29 00:01:04 +0300

    added save method

--- webStudioBeauty/webStudio/Services/CosmetologyService.cs
+++ webStudioBeauty/webStudio/Services/CosmetologyService.cs
@@ -1,4 +1,7 @@
-﻿using webStudio.Data;
+﻿using Microsoft.EntityFrameworkCore;
+
+using webStudio.Data;
+using webStudio.Models;
 
 namespace webStudio.Services
 {
@@ -8,6 +11,28 @@ namespace webStudio.Services
         public CosmetologyService(ApplicationDbContext applicationDbContext)
         {
             _applicationDbContext = applicationDbContext;
+        }
+
+        public void Save(CosmetologyTherapy cosmetology)
+        {
+            if (cosmetology.Id == 0)
+            {
+                var result = _applicationDbContext.CosmetologyTherapies.Add(cosmetology);
+                if (result.State == EntityState.Added)
+                {
+                    _applicationDbContext.SaveChanges();
+                }
+            }
+            else
+            {
+                var categoryInDbEntuty = _applicationDbContext.CosmetologyTherapies.Find(cosmetology.Id);
+                if (categoryInDbEntuty != null)
+                {
+                    _applicationDbContext.CosmetologyTherapies.Update(cosmetology);
+                    _applicationDbContext.SaveChanges();
+                }
+            }
+            _applicationDbContext.SaveChanges();
         }
     }
 }
